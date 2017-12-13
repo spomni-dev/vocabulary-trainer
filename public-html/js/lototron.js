fe1418f7f4e77560a70079b15b8714992ffa32fb
spomni <mikhail.snitko@gmail.com>
2017-12-13 03:33:06 +0300

    Comment code 	./index.html 	./css/style.css 	./js/lototron.js

--- public-html/js/lototron.js
+++ public-html/js/lototron.js
@@ -1,53 +1,60 @@
-try {
-
-  /* Construct an object that return unique random numbers from defined set.
-  * Обьект лототрон предназначен для выдачи в произвольном порядке целых чисел из заданного интервала. 
-  * Число не может повториться, пока не будут выданы все числа из заданного интераала.
-  * @constructor
-  
-  * @param {number} maxNumber
-  
-  * @method {number|string} getNextNumber - Retern next number from numbet array. Delete returned numbet from number array.
-  * @method {void} reload - Put all deleted numbers into number array.
-  * @method {array} getRestNumbers - Return array of the numbars that didn't be returned from method #getNextNumber.
-  
-  * @return {object}
-  */
+/** @class Lototron - Construct an object that return unique random numbers from defined set.
+ * @description -  Обьект лототрон предназначен для выдачи в произвольном порядке целых чисел из заданного интервала. Число не может повториться, пока не будут выданы все числа из заданного интераала.
+ * @param {number} maxNumber
+ * @returns {Lototron|Error}
+ */
   function Lototron(maxNumber){
     //-- Validate input params
-      if ( typeof(maxNumber) != 'number' ){
-        throw new Error("Wrong input param. maxNumber must be number.")
+      //-- If maxNumber is not number or < 0 then return Error.
+        if ( typeof(maxNumber) != 'number' ){
+          return new Error("Wrong input param. maxNumber should be number.")
+        }
+        if ( maxNumber < 0 ){
+          return new Error("Wrong input param. maxNumber should be >= 0.")
+        }
+    //--
+    /** @private @var {Lototron} self - symlink for this */
+      var self = this;
+    /** @private @var {array} numbersArray - array that contains all numbers */
+      var numbersArray = createNumbersArray(maxNumber);
+    /** @private @var {array} restNumbersArray - array that contains rest numbers */
+      var restNumbersArray = createNumbersArray(maxNumber);
+    //--
+    /** @method getNextNumber - return next random number
+     * @public
+     * @description - Return next number and delete it from restNumbersArray. If method don't can return an unique number then return null
+     * @returns {number|string}
+     */
+      self.getNextNumber = function (){
+        if (restNumbersArray.length <= 0){
+          return null;
+        } else {
+          var numberIndx = randomInteger(0, restNumbersArray.length-1);
+          return restNumbersArray.splice(numberIndx, 1)[0];
+        }
       }
-      
-    var self = this;
-    var numbersArray = createNumbersArray(maxNumber);
-    var restNumbersArray = createNumbersArray(maxNumber);
     
-    /* @method getNextNumber - return next random number
-    * @description - Return next random number and delete a returned number from arrau of numbers that will can be used on the futured callings of this function. If numbets is ended on previos callings then return string "Lototron is empty".
-    * @return {number|string}
-    */
-    self.getNextNumber = function (){
-      if (restNumbersArray.length <= 0){
-        return "Lototron is empty";
-      } else {
-        var numberIndx = randomInteger(0, restNumbersArray.length-1);
-        return restNumbersArray.splice(numberIndx, 1)[0];
+    /** @method reload - refresh restNumberArray to inital array.
+     * @public
+     * @returns {undefined}
+     */
+      self.reload = function(){
+        for ( var i=0; i<numbersArray.length; i++ ){
+          restNumbersArray[i] = i;
+        }
       }
-    }
-    
-    /* @method reload - refresh restNumberArray to inital array.*/
-    self.reload = function(){
-      for ( var i=0; i<numbersArray.length; i++ ){
-        restNumbersArray[i] = i;
+
+    /** @method getRestNumbers - return numbers that didn't was return from method getNextNumber
+     * @returns {array}
+     */
+      self.getRestNumbers = function(){
+        return restNumbersArray;
       }
-    }
-    /* @method getRestNumbers - return numbers from set that didn't be returned from method getNextNumber*/
-    self.getRestNumbers = function(){
-      return restNumbersArray;
-    }
-    
-    /* @function createNumbersArray - return array filled of the integer numbers from 0 to maxNumber. */
+    //
+    /** @function createNumbersArray - return array filled of the integer numbers from 0 to maxNumber. 
+     * @param {number} maxNumber
+     * @returns {array}
+     */
       function createNumbersArray( maxNumber ){
         var res = [];
         for ( var i=0; i<=maxNumber; i++ ){
@@ -55,73 +62,14 @@ try {
         }
         return res;
       }
-    
-    /* @function randomInteger - return randon number from min to max*/
+    /** @function randomInteger - return random number from min to max
+     * @param {number} min
+     * @param {number} max
+     * @returns {number}
+     */
       function randomInteger(min, max) {
         var rand = min + Math.random() * (max + 1 - min);
         rand = Math.floor(rand);
         return rand;
       } // randomInteger 
-
   }
-
-  // testLototron();
-  function testLototron(){
-    var _loto;
-    //-- test wrong input params
-      log("Test Lototron(): Wrong input params;");
-      // _loto = new Lototron();
-      log("\tSkip this test.");
-      log("----------------------");
-    //-- test method #getNextNumber()
-      log("Test Lototron.getNextNumber() - returned data type");
-        var maxNumber = 9;
-        _loto = new Lototron( maxNumber );
-        var temp = "";
-        for (var indx=0; indx < maxNumber + 3; indx++){
-          var res = _loto.getNextNumber();
-          temp += typeof( res ) + ", ";
-          if ( indx <= maxNumber ){
-            if ( typeof( res ) != 'number' ){
-              throw new Error("Returned invalid datatype from _loto.getNextNumber(). Result must be number.");
-            }
-          } else {
-            if ( typeof( res ) != 'string' ){
-              throw new Error("Returned invalid datatype from _loto.getNextNumber(). Result must be string.");
-            }
-          }
-        }
-        log("\tAll numbers: "+temp);
-      log("----------------------");
-
-      log("Test Lototron.getNextNumber() - random");
-        var maxNumber = 9;
-        log("\tMaxNumber ="+ maxNumber);
-        _loto = new Lototron( maxNumber );
-        var temp = "";
-        for (var indx=0; indx < maxNumber + 3; indx++){
-          temp += _loto.getNextNumber()+", ";
-        }
-        log("\tAll numbers: "+temp);
-      log("----------------------");
-    //-- Test Lototron.reload()
-      log("Test Lototron.reload()\n");
-          log("\tMaxNumber ="+ maxNumber+"\n");
-      _loto.reload();
-      for (var i=0; i<6; i++){
-        _loto.getNextNumber();
-      }
-      log("\trestNumbersArray before reload:");
-      log("\t"+_loto.getRestNumbers()+"\n");
-      _loto.reload();
-      log("\trestNumbersArray after reload:");
-      log("\t"+_loto.getRestNumbers()+"\n");
-    //--
-  }
-
-} catch(e){
-  log(e.message);
-  log(e.stack);
-} finally {
-  // alert("          log.viewportBufer:\n\n"+log.viewportBufer);
-}
