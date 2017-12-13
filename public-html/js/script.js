b30a4e4dec17d2391b3753f19e255cd64528fedd
spomni <mikhail.snitko@gmail.com>
2017-12-13 04:14:11 +0300

    Comment code 	./js/script.js 	./js/vocabulary-trainer.js

--- public-html/js/script.js
+++ public-html/js/script.js
@@ -1,10 +1,5 @@
-var log = new AnWritterLogger(".log-viewport");
 var bem = new BEM();
+
 document.addEventListener("DOMContentLoaded", function(){
-  try {
-    var vocabulary = new VocabularyTrainer( vocabularyDB );
-  } catch (exception) {
-    // log(exception.message);
-    if (exception.stack) log( exception.stack );
-  }
+  var vocabulary = new VocabularyTrainer( vocabularyDB );
 });
--- public-html/js/vocabulary-trainer.js
+++ public-html/js/vocabulary-trainer.js
@@ -7,84 +7,71 @@
     vocabulary__next-item-button
 */
 
-/* @function VocabularyTrainer - Конструктор объекта, управляющего модулем vocabulary-trainer.
-  * @constructor
-  *
-  * @param {array} vocabularyDB - Массив-БД, состоящий из подмассивов, каждый из которых содержит значения всех полей для конкретного слова(словосочетания)
-  * 
-  * @private {VacabularyTrainer} self - Simlink for variable "this".
-  * @private {array} _vocabularyDB - Simlшnk for the input param "vocabularyDB"
-  * @private {NodeList} _fieldsNodes - Instance of NodeList, содержащий список html-елементов, соответствующих полям модуля vocabulary-trainer.
-  * @private {Lototron} _loto - Экземпляр класса Lototron.
-  * 
-  * @private {void} #_changeVocabularyItem - Обработчик нажатия кнопки .vocabulary__next-item-button. Скрывает значения некоторых полей и заменяет значения всех полей значениями, соответствующими очередному словосочетанию.
-  * @private {void} #_showNextFieldValue - Обраьотчик нажатия кнопки .vocabulary__next-field-button. Делает видимым значение очередного поля модуля vocabulary-trainer.
-  * 
-  * @return {object}
-  *
-  * @require Lototron - Конструктор объекта, выдающего, по команде, случайное число из предопределенного диапазона. Возвращаемое значение не может повторяться.
-  * @require BEM v1.0p в переменной bem - Объект-библиотека для работы с сущностями подчиненными методологии БЭМ.
+/** @class VocabularyTrainer - Construct an object that control vocabulary-trainer module 
+ * @requires Lototron - Construct an object that return unique random numbers from defined set
+ * @requires BEM v0.1p as var bem - object-library for working on BEM methodology
+ * @param {array} vocabularyDB - Database array that contains subarrays. Each subarray contains values of vocabulary field.
+ * @returns {VocabularyTrainer|Error}
 */
 function VocabularyTrainer( vocabularyDB ){
   //-- Check input params
+    //-- If vocabularyDB is not array then return error
     if ( !(vocabularyDB instanceof Array) ){
-      throw new Error("Wrong input param. VocabularyDB must be instanceof Array");
+      return new Error("Wrong input param. VocabularyDB must be instanceof Array");
     }
-  //-- Define private variables
+  //
+  /** @private @var {VocabularyTrainer} self - Symlink for this */
     var self = this;
+  /** @private @var {array} _vocabularyDB - array of vocabulary database*/
     var _vocabularyDB = vocabularyDB;
+  /** @private @var _fieldsNodes - List of vocabulary fields nodes. */
     var _fieldsNodes = document.querySelectorAll( ".vocabulary__field.field" );
+  /** @private @var _loto - Instance of Lototron. Use for generate next item. */
     var _loto = new Lototron( _vocabularyDB.length - 1 );
-  
-  //-- Init nodes
+  //
+  /** @method _changeVocabularyItem - .vocabulary__next-item-button handler
+   * @description Hide some fields value and replace values of all fields for next vocabulary item.
+   * @returns {void}
+   */
+    function _changeVocabularyItem(){
+      //-- getItemNumber
+        var itemNumber = _loto.getNextNumber();
+        if ( typeof(itemNumber) != "number" ){
+          _loto.reload();
+          itemNumber = _loto.getNextNumber();
+        }
+      //-- hideFieldsValues
+        [].forEach.call(_fieldsNodes, function(elem, indx, array){
+          if (indx > 0) {
+            bem.lib.setModifier(elem, "field_hidden-value_true");
+          }
+        });
+      //-- replaceFieldsValues
+        var fieldsValuesDB = _vocabularyDB[itemNumber];
+        [].forEach.call(_fieldsNodes, function(elem, indx, array){
+          var fieldValueNode = elem.querySelector(".field__value");
+          fieldValueNode.innerHTML = fieldsValuesDB[indx];
+        });
+    }
+
+  /** @method _showNextFieldValue - .vocabulary__next-field-button handler
+   * @description Show value of next vocabulary field
+   * @returns {void}
+   */
+    function _showNextFieldValue(){
+      for (var i=1; i<_fieldsNodes.length; i++){
+        var modifierValue = bem.lib.getModifierValue( _fieldsNodes[i], "field_hidden-value" );
+        if ( modifierValue === "true"){
+          bem.lib.setModifier( _fieldsNodes[i], "field_hidden-value_false" );
+          break;
+        }
+      }
+    }
+  //
+  //-- Fill html nodes of fields
     _changeVocabularyItem();
-  
+  //-- Attach handlers to buttons
     document.querySelector(".vocabulary__next-field-button").addEventListener("click", _showNextFieldValue);
     document.querySelector(".vocabulary__next-item-button").addEventListener("click", _changeVocabularyItem);
-  //
-  /* @method _changeVocabularyItem - Обработчик нажатия кнопки .vocabulary__next-item-button.
-   * @private
-   *
-   * @description - Скрывает значения некоторых полей и заменяет значения всех полей значениями, соответствующими очередному словосочетанию.
-   *
-   * @return {void}
-  */
-  function _changeVocabularyItem(){
-    //-- getItemNumber
-      var itemNumber = _loto.getNextNumber();
-      if ( typeof(itemNumber) != "number" ){
-        _loto.reload();
-        itemNumber = _loto.getNextNumber();
-      }
-    //-- hideFieldsValues
-      [].forEach.call(_fieldsNodes, function(elem, indx, array){
-        if (indx > 0) {
-          bem.lib.setModifier(elem, "field_hidden-value_true");
-        }
-      });
-    //-- replaceFieldsValues
-      var fieldsValuesDB = _vocabularyDB[itemNumber];
-      [].forEach.call(_fieldsNodes, function(elem, indx, array){
-        var fieldValueNode = elem.querySelector(".field__value");
-        fieldValueNode.innerHTML = fieldsValuesDB[indx];
-      });
-  }
-  //
-  /* @method _showNextFieldValue - Обработчик нажатия кнопки .vocabulary__next-field-button.
-   * @private
-   *
-   * @description - Делает видимым значение очередного поля модуля vocabulary-trainer.
-   * 
-   * @return {void}
-  */
-  function _showNextFieldValue(){
-    for (var i=1; i<_fieldsNodes.length; i++){
-      var modifierValue = bem.lib.getModifierValue( _fieldsNodes[i], "field_hidden-value" );
-      if ( modifierValue === "true"){
-        bem.lib.setModifier( _fieldsNodes[i], "field_hidden-value_false" );
-        break;
-      }
-    }
-  }
-  
+  //  
 }
